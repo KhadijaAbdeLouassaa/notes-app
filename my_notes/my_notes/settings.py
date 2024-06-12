2e827292e9a3a5dc3d05720bb5feb3e30bf770e6
KAL <kal.codes00@gmail.com>
2024-06-12 20:03:21 +0000

    _

--- my_notes/my_notes/settings.py
+++ my_notes/my_notes/settings.py
@@ -118,13 +118,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR/"media" 
 
 
 
